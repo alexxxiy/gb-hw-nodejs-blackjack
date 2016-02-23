83f5e9dc3862d747edfe1776999c6776a2d1c2ea
Alexey Alekhin <clockwork_orange@mail.ru>
2016-02-23 17:31:25 +0300

    correct index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var game = require('./black_jack');
+
 game.initGame();
 
-// var fs = require('fs');
 
-// fs.readFile('./log/alex_log.txt', 'utf8', (err, data) => {
-// 	if (err) throw err;
-// 	console.log(data.split('\n'));
-// });
 
